f9a14f5c8fcdf91db83a6f864d5303f0dbfa5dfa
Boyzrandom <adielboyel@gmail.com>
2026-01-30 15:45:18 +0000

    Fix: Add RootLayout with correct tags

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,13 @@
+// Gunakan 'export' huruf kecil
 export const metadata = {
   title: 'Crypto Analysis Elsa',
-  description: 'Testing Dashboard Crypto di Replit',
+  description: 'Testing Dashboard Crypto di Vercel',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, backgroundColor: '#121212' }}>
+      <body style={{ margin: 0, backgroundColor: '#121212', color: 'white' }}>
         {children}
       </body>
     </html>
